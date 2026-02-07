1d70886f45aed8aecec9bc3395a694e55c51f683
Anarion Technologies <sainig273@gmail.com>
2026-02-07 15:26:35 +0530

    Initial Commit

--- main.tf
+++
@@ -1,8 +0,0 @@
-provider "aws" {
-  region = "eu-north-1" 
-}
-
-resource "aws_instance" "example" {
-  ami           = var.ami_value
-  instance_type = var.instance_type_value
-}
--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "ami_value" {
-  description = "AMI ID for EC2 instance"
-  type        = string
-}
-
-variable "instance_type_value" {
-  description = "EC2 instance type"
-  type        = string
-}
